6868d331dd90a5c308a8ae65e5173c79383595f9
Rafael305 <fraq86@gmail.com>
2025-08-01 11:18:24 -0300

    object fit

---
+++ Animaciones/codigo.js
@@ -0,0 +1,4 @@
+localStorage.setItem('Nombre','ken');
+
+//Otros metodos, getItem(key), removeItem(key)
+//Para sessionStorage los metodos son los mismos, icluido uno llamaod clera que tambien esta en localStorage.
