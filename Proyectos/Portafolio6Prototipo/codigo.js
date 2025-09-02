ede4f5a9dedae8acfd55e4e455ad33445d00d942
Rafael305 <fraq86@gmail.com>
2025-09-02 10:35:59 -0300

    asignacion de cuatro tipos de fuente

--- Proyectos/Portafolio6Prototipo/codigo.js
+++ Proyectos/Portafolio6Prototipo/codigo.js
@@ -2,6 +2,7 @@ const botonColores = document.getElementById("boton-colores")
 const botonFuentes = document.getElementById("boton-fuentes")
 const body = document.querySelector('body')
 const botonTema = document.querySelectorAll('.boton-tema')
+const nombreCompleto = document.getElementById('nombre-completo')
 const letrasColorPrincipal = document.querySelectorAll('.letras-color-principal')
 const texto = document.querySelectorAll('.texto')
 const fondoColorGradiente = document.querySelectorAll('.fondo-color-gradiente')
@@ -10,6 +11,7 @@ const letrasFondo = document.querySelectorAll('.letras-fondo')
 const proyecto = document.querySelectorAll('.proyecto')
 const enlace = document.querySelectorAll('.enlace')
 const enlaceNumerado = document.querySelectorAll('.enlace-numerado')
+const sonidoBoton = new Audio('sonido-boton.wav')
 
 let temaFuente = 0
 
@@ -43,28 +45,40 @@ function cambiarTema(){
     for(elemento of enlace){
         elemento.classList.toggle('enlace-estilo-2')
     }
+    sonidoBoton.currentTime = 0
+    sonidoBoton.play()
 }
 
 function cambiarFuente(){
     if(temaFuente === 0){
         body.classList.remove('letra-orbitron')
         body.classList.add('letra-roboto')
+        nombreCompleto.classList.add('nombre-completo-roboto')
         temaFuente++
     }else if(temaFuente === 1){
         body.classList.remove('letra-roboto')
+        nombreCompleto.classList.remove('nombre-completo-roboto')
         body.classList.add('letra-quintessential')
+        nombreCompleto.classList.add('nombre-completo-quintessential')
         temaFuente++
     }
     else if(temaFuente === 2){
-        body.classList.remove('.letra-quintessential')
-        body.classList.add('letra-pacifico')
+        body.classList.remove('letra-quintessential')
+        nombreCompleto.classList.remove('nombre-completo-quintessential')
+        body.classList.add('letra-parisienne')
+        nombreCompleto.classList.add('nombre-completo-parisienne')
         temaFuente++
     }else if(temaFuente === 3){
-        body.classList.remove('letra-pacifico')
+        body.classList.remove('letra-parisienne')
+        nombreCompleto.classList.remove('nombre-completo-parisienne')
         body.classList.add('letra-orbitron')
         temaFuente = 0
     }
+    sonidoBoton.currentTime = 0
+    sonidoBoton.play()
+    // console.log('Tema fuente: ',temaFuente)
 }
+
 
 botonColores.addEventListener("click",cambiarTema)
 botonFuentes.addEventListener("click",cambiarFuente)
